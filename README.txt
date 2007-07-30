56fdd7f2ff6e829cb049202806bf42b7bbc6101e
Matt Zukowski <matt@zukowski.ca>
2007-07-30 22:11:01 +0000

    cleaned up documentation, added missing LICENSE, ready for distribution

--- README.txt
+++ README.txt
@@ -11,22 +11,22 @@ You can contact the author at:
   matt at roughest dot net
   
 
-<b>Reststop makes it easy to write RESTful[http://en.wikipedia.org/wiki/Representational_State_Transfer]
+<b>Reststop makes it easy to write RESTful[http://en.wikipedia.org/wiki/Representational_State_Transfer] 
 applications in Camping[http://camping.rubyforge.org/files/README.html].</b>
 
 Reststop essentially gives you two things:
 
-=== Camping controllers that respond to the standard REST verbs:
+<b>Camping controllers that respond to the standard REST verbs:</b>
 
-  * create (POST)
-  * read (GET)
-  * update (PUT)
-  * destroy (DELETE)
-  * list (GET)
+* create (POST)
+* read (GET)
+* update (PUT)
+* destroy (DELETE)
+* list (GET)
 
-See the REST method documentation for usage info.
+See the Camping::Controllers#REST method documentation for usage info.
 
-=== Camping views grouped by output format:
+<b>Camping views grouped by output format:</b>
 
 Your views module:
 
@@ -52,3 +52,19 @@ Your render call:
   render(:foo, :XML)
   
 See the render() method documentation for usage info.
+
+
+----
+
+Reststop is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Reststop is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- lib/reststop.rb
+++ lib/reststop.rb
@@ -1,3 +1,21 @@
+#--
+# This file is part of Reststop.
+#
+# Reststop is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Reststop is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--
+
+
 module Reststop
   unless const_defined? 'VERSION'
   	module VERSION #:nodoc:
@@ -11,71 +29,32 @@ module Reststop
 end
 
 
-# Convenient RESTfulness for all your Camping controller needs.
+# Extends and overrides Camping for convenient RESTfulness.
 #
-# To use, call `REST '<resource name>'` instead of `R <routes>` in your 
-# Controllers module, then define methods for the standard CRUD verbs
-# ('create', 'read', 'update', 'delete') and also  'list'. 
-# 
-# Routes will be created automatically based on the parameters fed to 
-# REST. Note that your class must have the same (but CamelCaps'ed) name as
-# the resource name to provide to REST. So if you said 'kittens', your 
-# controller class must be 'Kittens'.
+# Have a look at:
 #
-# For example:
+# * Camping::Controllers#REST for help on using RESTful controllers
+# * Camping#render for help on grouping your views by output format
 #
-#   module Foobar::Controllers
-#     class Items < REST 'items'
-#       # POST /items
-#       def create
-#       end
-#
-#       # GET /items/(\d+)
-#       def read(id)
-#       end
-#
-#       # PUT /items/(\d+)
-#       def update(id)
-#       end
-#
-#       # DELETE /items/(\d+)
-#       def destroy(id)
-#       end
-#
-#       # GET /items
-#       def list
-#       end
-#     end
-#   end
-#
-#
-# Optionally, you can specify a :prefix option that will prepend this
-# string to the routes. For example, this will create all of the above
-# routes, prefixed with '/stuff' (i.e. POST '/stuff/items', 
-# GET '/stuff/items/(\d+)', etc.)
-#
-#   module Foobar::Controllers
-#     class Items < REST 'items', :prefix => '/stuff'
-#       # ...
-#     end
-#   end
-
-
 module Camping
   
   # Override Camping's goes() mechanism so that we can add our stuff.
   # ... there must be a saner way to do this >:|
-  S2 = IO.read(__FILE__).gsub(/^  S2 = I.+$/,'')
+  #
+  # FIXME: looks like this breaks auto-reloading when using the camping
+  #        server for launching apps :(
+  S2 = IO.read(__FILE__).gsub(/^  S2 = I.+$/,'') # :nodoc:
   class << self
-    alias_method :camping_goes, :goes
-    def goes(m)
+    alias_method :camping_goes, :goes 
+    def goes(m) # :nodoc:
       camping_goes m
       eval S2.gsub('Camping', m.to_s), TOPLEVEL_BINDING
     end
   end
   
-  # This snippet is stolen and slightly modified from the Camping mailing list;
-  # it fakes PUT/DELETE HTTP methods, since many browsers don't support this.
+  # This override is stolen and slightly modified from the Camping mailing list;
+  # it fakes PUT/DELETE HTTP methods, since many browsers don't support them.
+  #
   # In your forms you will have to add:
   #
   # input :name => '_method', :type => 'hidden', :value => 'VERB'
@@ -91,7 +70,8 @@ module Camping
     super(*a)
   end
   
-  # Adds ability to specify a format when rendering a view.
+  # Overrides Camping's render method to add the ability to specify a format 
+  # module when rendering a view.
   #
   # Format is specified in one of three ways (in this order of precedence):
   #
@@ -166,8 +146,19 @@ module Camping
     end
   end
 
+  # See Camping#render
   module Views
     class << self
+      # Call this inside your Views module to set a default format.
+      #
+      # For example:
+      #
+      #   module Foobar::Views
+      #     module HTML
+      #       # ... etc.
+      #     end
+      #     default_format :XML
+      #   end
       def default_format(m)
         mod = "Camping::Views::#{m.to_s}".constantize
         Mab.class_eval{include mod}
@@ -177,13 +168,66 @@ module Camping
   
   module Controllers
     class << self
-      # Calling `REST '<resource name>'` creates a controller with the
+      # Calling <tt>REST "<resource name>"</tt> creates a controller with the
       # appropriate routes and maps your REST methods to standard
-      # Camping controller mehods.
+      # Camping controller mehods. This is meant to be used in your Controllers
+      # module in place of <tt>R <routes></tt>.
+      #
+      # Your REST class should define the following methods:
+      # 
+      # * create
+      # * read(id)
+      # * update(id)
+      # * destroy(id)
+      # * list
+      # 
+      # Routes will be automatically created based on the resource name fed to the
+      # REST method. Note that your class must have the same (but CamelCaps'ed) 
+      # name as the resource name. So if your resource name is 'kittens', 
+      # your controller class must be Kittens.
+      #
+      # For example:
+      #
+      #   module Foobar::Controllers
+      #     class Kittens < REST 'kittens'
+      #       # POST /kittens
+      #       def create
+      #       end
+      #
+      #       # GET /kittens/(\d+)
+      #       def read(id)
+      #       end
+      #
+      #       # PUT /kittens/(\d+)
+      #       def update(id)
+      #       end
+      #
+      #       # DELETE /kittens/(\d+)
+      #       def destroy(id)
+      #       end
+      #
+      #       # GET /kittens
+      #       def list
+      #       end
+      #     end
+      #   end
+      #
+      #
+      # Optionally, you can specify a :prefix key that will prepend this
+      # string to the routes. For example, this will create all of the above
+      # routes, prefixed with '/pets' (i.e. POST '/pets/kittens', 
+      # GET '/pets/kittens/(\d+)', etc.)
+      #
+      #   module Foobar::Controllers
+      #     class Items < REST 'kittens', :prefix => '/pets'
+      #       # ...
+      #     end
+      #   end
       def REST(r, options = {})
         crud = R "#{options[:prefix]}/#{r}/(.+)", "#{options[:prefix]}/#{r}"
         crud.module_eval do
-          def get(id = nil)
+          
+          def get(id = nil) # :nodoc:
             if id.nil? && @input[:id].nil?
                list
             else
@@ -191,15 +235,18 @@ module Camping
             end
           end
           
-          def post   
+          
+          def post # :nodoc:
             create
           end
           
-          def put(id = nil)
+          
+          def put(id = nil) # :nodoc:
             update(id || @input[:id])
           end
           
-          def delete(id = nil)
+          
+          def delete(id = nil) # :nodoc:
             destroy(id || @input[:id])
           end
         end
